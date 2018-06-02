418fff1a6663f447037700d599190897c9c9fd66
scott <scott@tams.tech>
2018-06-02 14:51:00 -0400

    moved everything into functions, added main, added set_trusted domains, fixed infinite loop.

--- nextcloud/first_run.py
+++ nextcloud/first_run.py
@@ -2,6 +2,7 @@
 from docker     import DockerClient
 from jinja2     import Template
 from yaml       import load, dump
+from json       import loads, dumps
 from subprocess import check_call
 from time       import sleep
 from misc       import TerminalOutputModifiers
@@ -29,56 +30,76 @@ def wait(until, condition=lambda: False, throw=False):
     while i < until:
         if condition():
             return
+        i += 1
         sleep(1)
     if throw:
         raise throw()
 
 
-with open(u"docker-compose.yml.j2", u'r') as compose_file:
-    composition_text = Template(compose_file.read()).render(
-        {u"password": passwords[u'database']}
-    )
-with open(u"docker-compose.yml", u'w') as compose_file:
-    compose_file.write(composition_text)
+def compose():
+    with open(u"docker-compose.yml.j2", u'r') as compose_file:
+        composition_text = Template(compose_file.read()).render(
+            {u"password": passwords[u'database']}
+        )
+    with open(u"docker-compose.yml", u'w') as compose_file:
+        compose_file.write(composition_text)
 
-check_call(u"docker-compose up -d", shell=True)
-wait(
-    60,
-    lambda: len(client.containers.list(
+    check_call(u"docker-compose up -d", shell=True)
+    wait(30)
+    assert len(client.containers.list(filters={u"name": u"nextcloud_frontend_1"}))
+    return client.containers.list(
         filters={u"name": u"nextcloud_frontend_1"}
-    )),
-    ContainerDidNotStartException
-)
+    )[0]
 
 
-nextcloud_container = client.containers.list(
-    filters={u"name": u"nextcloud_frontend_1"}
-)[0]
-assert nextcloud_container.status == u"running"
-install_command = (
-    u'php occ maintenance:install --database "mysql" '
-    + u'--database-name "nextcloud" --database-user "nextcloud" '
-    + u'--database-host "nextcloud_database_1" --database-port "3306" '
-    + u'--database-pass "%s" --admin-user "scott" ' % passwords[u'database']
-    + u'--admin-pass "%s"' % passwords[u'admin']
-)
-install_result = nextcloud_container.exec_run(
-    install_command, user=u'www-data',
-)   # ^^ blocks until done.
-if install_result.exit_code:
-    print "Installation command: %s" % install_command
-    print "resulted in the following error:"
-    print install_result.output
-    exit(install_result.exit_code)
-check_call(u'docker-compose down && docker-compose up -d', shell=True)
-print(
-      font.build(FOREGROUND_COLOR=b"CYAN", WEIGHT=b"BOLD")
-    + b"The nextcloud container should be ready now."
-    + font.build(WEIGHT=b'NORMAL')
-    + b"Switching to Apache log output..."
-    + font.build(b'RESET')
-)
+def install_nextcloud(nextcloud_container):
+    assert nextcloud_container.status == u"running"
+    install_command = (
+        u'php occ maintenance:install --database "mysql" '
+        + u'--database-name "nextcloud" --database-user "nextcloud" '
+        + u'--database-host "nextcloud_database_1" --database-port "3306" '
+        + u'--database-pass "%s" --admin-user "scott" ' % passwords[u'database']
+        + u'--admin-pass "%s"' % passwords[u'admin']
+    )
+    install_result = nextcloud_container.exec_run(
+        install_command, user=u'www-data',
+    )   # ^^ blocks until done.
+    if install_result.exit_code:
+        print "Installation command: %s" % install_command
+        print "resulted in the following error:"
+        print install_result.output
+        exit(install_result.exit_code)
+    check_call(u'docker-compose down && docker-compose up -d', shell=True)
+    print(
+          font.build(FOREGROUND_COLOR=b"CYAN", WEIGHT=b"BOLD")
+        + b"The nextcloud container should be ready now."
+        + font.build(WEIGHT=b'NORMAL')
+        + b"Switching to Apache log output..."
+        + font.build(b'RESET')
+    )
 
-switch_to_cmd(u"docker-compose", u"docker-compose", u'logs', u'-f')
 
-help(Container.exec_run)
+def set_trusted_domains(nextcloud_container):
+    cmd_output = nextcloud_container.exec_run(
+        "php occ config:list", user="www-data"
+    )
+    if cmd_output.exit_code:
+        print "Error running command to get config."
+        exit(cmd_output.exit_code)
+    config = loads(cmd_output.output)
+    config['system']['trusted_domains'] = ["s.cloud.tams.tech"]
+    check_call("echo '%s' > /tmp/nextcloud.config.json" % dumps(config))
+    set_config_output = nextcloud_container.exec_run(
+        "php occ config:import /tmp/nextcloud.config.json"
+    )
+
+
+def main():
+    cont = compose()
+    install_nextcloud(cont)
+    set_trusted_domains(cont)
+
+
+if __name__ == '__main__':
+    main()
+    switch_to_cmd(u"docker-compose", u"docker-compose", u'logs', u'-f')
