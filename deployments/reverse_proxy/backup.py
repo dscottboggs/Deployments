6004a3b5232b41c63788079213d5eb4d061fab7c
D. Scott Boggs <scott@tams.tech>
2018-06-03 15:03:21 -0400

    basic rsync -> tar.gz backup

--- deployments/reverse_proxy/backup.py
+++ deployments/reverse_proxy/backup.py
@@ -1,10 +1,37 @@
 """Backup the certificates and configurations stored in ./mounts/"""
 from subprocess import check_call
-from time       import time
+from datetime   import datetime
+from tarfile    import TarFile
 
 
-test_backup_dir = "/home/scott/Documents/code/Deployments/test_backups"
-reverse_proxy_dir = '.'
-now = datetime.now().ctime()
-print now
-check_call("rsync -aqX --partial %(rpd)s/mounts/ %(tbd)s/staging/reverse_proxy/")
+class BackupReverseProxy:
+    """Methods for backing up the reverse proxy mounts."""
+    backup_dir = "/home/scott/Documents/code/Deployments/test_backups/reverse_proxy"
+    reverse_proxy_dir = '.'
+
+    def __init__(self):
+        """Initialize variables needed for all methods."""
+        self.now = datetime.now().strftime('%s')       # the current epoch time
+        self.stage = "%(bd)s/staging/%(t)s" % {
+            'bd': self.backup_dir, 't': self.now
+        }
+
+    def copy_files_to_stage(self):
+        """Copy the files themselves and their attrs to a staging area."""
+        check_call(
+            "rsync -aqX --partial %(rpd)s/mounts/ %(dest)s"
+            % {
+                'rpd': self.reverse_proxy_dir, 'dest': self.stage
+            },
+            shell=True
+        )
+
+    def archive(self):
+        """Put the folder in a gzipped tar archive."""
+        with TarFile.open(
+                    "%(backup)s/%(t)s.tar.gz" % {
+                        'backup': self.backup_dir, 't': self.now
+                    },
+                    mode='x:gz'     # Raises an exception if file exists, open
+                ) as tarchive:      # as gzipped file for writing.
+            tarchive.add(self.stage)
