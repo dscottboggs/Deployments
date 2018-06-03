067c47a7c2ac8b1ee8190d0e60d073a9676634fa
scott <scott@tams.tech>
2018-06-02 21:24:17 -0400

    Added multi-user config

--- nextcloud/first_run.py
+++ nextcloud/first_run.py
@@ -5,13 +5,13 @@ from yaml       import load, dump
 from json       import loads, dumps
 from subprocess import check_call
 from time       import sleep
-from misc       import TerminalOutputModifiers
+from misc       import TerminalOutputModifiers, occ
 from os         import execlp                                  as switch_to_cmd
 
 client = DockerClient(u"unix://var/run/docker.sock", version=u"1.30")
 font   = TerminalOutputModifiers()
-with open(u"passwords.yml", u'r') as password_file:
-    passwords = load(password_file)
+with open(u"user.yml", u'r') as uInfo_file:
+    user_info = load(uInfo_file)
 
 
 class ContainerDidNotStartException(Exception): pass
@@ -21,7 +21,7 @@ def wait(until, condition=lambda: False, throw=False):
     u"""Wait for `until` seconds, or for `condition` to be true.
 
     By default `condition` is false, causing this function to wait the whole
-    time specified by until.
+    time specified by until. The `condition` parameter must be a callable.
 
     If throw is a truthy value, it's expected to be a throwable callable,
     i.e. the constructor of a class that extends Exception.
@@ -37,9 +37,24 @@ def wait(until, condition=lambda: False, throw=False):
 
 
 def compose():
+    """Create the docker-compose file from the template and call 'up' on it."""
+    urls = ''
+    if user_info.get('urls'):
+        print("At least one URL must be specified.")
+        exit(1)
+    elif len(user_info['urls']) == 1:
+        urls = user_info['urls'][0]
+    else:
+        for url in user_info['urls'][:-1]:
+            urls += url + ','
+        urls += user_info['urls'][-1]
     with open(u"docker-compose.yml.j2", u'r') as compose_file:
         composition_text = Template(compose_file.read()).render(
-            {u"password": passwords[u'database']}
+            {
+                u"password":    user_info[u'database'],
+                u"url":         urls,
+                u"admin_email": user_info[u'admin'][0]['email']
+            }
         )
     with open(u"docker-compose.yml", u'w') as compose_file:
         compose_file.write(composition_text)
@@ -54,40 +69,71 @@ def compose():
 
 def install_nextcloud(nextcloud_container):
     assert nextcloud_container.status == u"running"
-    install_command = (
-        u'php occ maintenance:install --database "mysql" '
-        + u'--database-name "nextcloud" --database-user "nextcloud" '
-        + u'--database-host "nextcloud_database_1" --database-port "3306" '
-        + u'--database-pass "%s" --admin-user "scott" ' % passwords[u'database']
-        + u'--admin-pass "%s"' % passwords[u'admin']
-    )
-    install_result = nextcloud_container.exec_run(
-        install_command, user=u'www-data',
-    )   # ^^ blocks until done.
-    if install_result.exit_code:
-        print "Installation command: %s" % install_command
-        print "resulted in the following error:"
-        print install_result.output
-        exit(install_result.exit_code)
+    if not occ(
+                "maintenance:install",
+                '--database="mysql"',
+                '--database-name="nextcloud"',
+                '--database-user="nextcloud"',
+                '--database-host="nextcloud_database_1"',
+                '--database-pass="%s"' % user_info[u'database']
+                '--admin-user="%s"' % user_info[u'admin'][0][u'user_id'],
+                '--admin-pass="%s"' % user_info[u'admin'][0][u'password']
+            ):
+        print "Install command failed."
+        exit(1)
     check_call(u'docker-compose down && docker-compose up -d', shell=True)
     return client.containers.list(
         filters={u"name": u"nextcloud_frontend_1"}
     )[0]
 
 def set_trusted_domains(nextcloud_container):
+    """Use sed(1) to change the URL of the config.php file.
+
+    TODO handle more than one URL.
+    """
     cmd_output = nextcloud_container.exec_run(
-        "sed -i s/localhost/s.cloud.tams.tech/ config/config.php"
+        "sed -i s/localhost/%s/ config/config.php" % user_info['urls'][0]
     )
     if cmd_output.exit_code:
         print "Error running the command to update the config:"
         print cmd_output.output
         exit(cmd_output.exit_code)
 
+def setup_users():
+    """Set up the info for each user specified in the user.yml file."""
+    for admin in user_info['admin']:
+        if not occ(None, "user:info", admin['user_id']):
+            occ(
+                {"OC_PASS": admin['password']},
+                'user:create', "--password-from-env",
+                '--display-name="%s"' % admin['display_name'],
+                '--group="admin"'
+                admin['user_id'],
+            )
+            print "Added user %s. Please remember to set email in the GUI."\
+                % admin['display_name']
+    for user in user_info['others']:
+        if not occ(None, "user:info", user['user_id']):
+            occ(
+                {"OC_PASS": user['password']},
+                'user:create', "--password-from-env",
+                '--display-name="%s"' % user['display_name'],
+                user['user_id'],
+            )
+            print "Added user %s. Please remember to set email in the GUI."\
+                % user['display_name']
+
 
 def main():
+    """Perform each of the installation steps.
+
+    Compose the services, install nextcloud, set the trusted domains,
+    and setup the users.
+    """
     cont = compose()
     cont = install_nextcloud(cont)
     set_trusted_domains(cont)
+    setup_users(cont)
 
 
 if __name__ == '__main__':
--- nextcloud/test_nextcloud.py
+++ nextcloud/test_nextcloud.py
@@ -4,63 +4,6 @@ from first_run  import client
 from yaml       import load
 
 
-def occ(environment, *cmdargs):
-    """Execute the `php occ` command in the container.
-
-    Arguments should be specified as strings passed to this function,
-    for example:
-        occ("user:add", "--display-name", "Scott", "scott@tams.tech")
-
-    If environment is specified it should be a list of "ENV=value" or
-    a dict mapping the environment variable to its value. If you don't
-    want to specify an environment, you have to specify a falsey value,
-    like None, or the empty string ('').
-
-    Commands are run as the www-data user.
-    """
-    def get_environ():
-        cmd = ''
-        if environment:
-            if isinstance(environment, dict):
-                for k, v in environment.items():
-                    cmd += "%s=%s" % k, v
-                return cmd
-            elif isinstance(environment, list):
-                for env in environment:
-                    cmd += env
-                return cmd
-            else:
-                raise TypeError(
-                    "Environment must be a list or dict, got %s of type %s."
-                    % environment, type(environment)
-                )
-        else:
-            return ''
-    container = client.containers.list(
-        filters={"name": "nextcloud_frontend_1"})[0]
-    cmd = get_environ() + " php occ"
-    for arg in cmdargs:
-        if isinstance(arg, str):
-            cmd += ' ' + arg
-        else:
-            raise TypeError(
-                "Each argument must be a string, got %s of type %s." % (
-                    arg, type(arg)
-                )
-            )
-    result = container.exec_run(cmd, user="www-data")
-    if result.exit_code:
-        print "ERROR while running the following command in the"
-        print "nextcloud container:"
-        print cmd
-        print "The command output the following:"
-        print result.output
-        print "and executed with the code %d." % result.exit_code
-        return False
-    else:
-        return result.output
-
-
 def test_index():
     """Test that the root URLs respond properly."""
     response = get("http://s.cloud.tams.tech")
