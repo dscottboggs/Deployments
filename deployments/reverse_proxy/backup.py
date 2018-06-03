2153213b8734c101c6a10ae8572651454741ad79
D. Scott Boggs <scott@tams.tech>
2018-06-03 15:26:10 -0400

    Added generic rsync backup class and main func

--- deployments/reverse_proxy/backup.py
+++ deployments/reverse_proxy/backup.py
@@ -4,24 +4,26 @@ from datetime   import datetime
 from tarfile    import TarFile
 
 
-class BackupReverseProxy:
-    """Methods for backing up the reverse proxy mounts."""
-    backup_dir = "/home/scott/Documents/code/Deployments/test_backups/reverse_proxy"
-    reverse_proxy_dir = '.'
+class BasicRsyncBackup:
+    """Methods for backing up a folder by using the `rsync`(1) command.
 
-    def __init__(self):
-        """Initialize variables needed for all methods."""
-        self.now = datetime.now().strftime('%s')       # the current epoch time
-        self.stage = "%(bd)s/staging/%(t)s" % {
-            'bd': self.backup_dir, 't': self.now
-        }
+    Subclasses must have the following attributes:
+        - backup_dir
+        - stage
+        - source_dir
+    """
+
+    def do_backup(self):
+        """Perform each step in the backup procedure."""
+        self.copy_files_to_stage()
+        self.archive()
 
     def copy_files_to_stage(self):
         """Copy the files themselves and their attrs to a staging area."""
         check_call(
             "rsync -aqX --partial %(rpd)s/mounts/ %(dest)s"
             % {
-                'rpd': self.reverse_proxy_dir, 'dest': self.stage
+                'rpd': self.source_dir, 'dest': self.stage
             },
             shell=True
         )
@@ -35,3 +37,26 @@ class BackupReverseProxy:
                     mode='x:gz'     # Raises an exception if file exists, open
                 ) as tarchive:      # as gzipped file for writing.
             tarchive.add(self.stage)
+
+
+class BackupReverseProxy(BasicRsyncBackup):
+    """Methods for backing up the reverse proxy mounts."""
+    backup_dir = "/backup/reverse_proxy"
+    source_dir = '.'
+
+    def __init__(self):
+        """Initialize variables needed for all methods."""
+        self.now = datetime.now().strftime('%s')       # the current epoch time
+        self.stage = "%(bd)s/staging/%(t)s" % {
+            'bd': self.backup_dir, 't': self.now
+        }
+
+
+def main():
+    """Code to execute this file as a script."""
+    backup = BackupReverseProxy()
+    backup.do_backup()
+
+
+if __name__ == '__main__':
+    main()
