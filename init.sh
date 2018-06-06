bc8186101970b43627ed86ebe0b01ca49a70491a
scott <scott@tams.tech>
2018-06-05 22:27:24 -0400

    Added init.sh to set up the repo itself and dependencies on centos

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+yum install epel-release
+yum install python34-pip python34-docker python34-pytest python34-PyYAML      \
+            python34-jinja2 docker docker-compose
+mkdir /home/scott/.ssh
+ssh-keygen -t ed25519 -f .ssh/id_ed25519 -q
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICa1xQPW/kFnPrO51Mp5gWpEpRZO8d6vtrWxIIpOoFd4 scott@scotts-server
+' > /home/scott/.ssh/authorized_keys
+cd /home/scott && \
+    git clone --recursive https://github.com/dscottboggs/Deployments.git
+cd /home/scott/Deployments      &&\
+    python34 setup.py install   &&\
+    python34 run.py
