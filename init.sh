87d2d20bb9a43d8d7b4aea2052444d0e42181834
D. Scott Boggs <scott@tams.tech>
2018-06-06 12:19:32 -0400

    Updated init.sh to get the latest versions on Ubuntu or CentOS

--- init.sh
+++ init.sh
@@ -1,7 +1,36 @@
 #!/usr/bin/env bash
-yum install epel-release
-yum install python34-pip python34-docker python34-pytest python34-PyYAML      \
-            python34-jinja2 docker docker-compose vim zsh rsync git
+
+function install_ubuntu() {
+    apt install -y python3-pip python3-docker python3-pytest python3-pyyaml \
+        python3-jinja2 vim zsh rsycn git apt-transport-https ca-certificates \
+        software-properties-common
+    wget -qO - https://get.docker.com | sh\
+        && service docker enable \
+        && service docker start \
+        || returncode=$? \
+            && echo "problem installing docker!" \
+            && exit $returncode
+    python3 -m pip install -U pip
+    python3 -m pip install docker-compose
+}
+function install_centos() {
+    yum install -y epel-release
+    yum install -y python34-pip python34-docker python34-pytest \
+        python34-PyYAML python34-jinja2 vim zsh rsync git
+    wget -qO - https://get.docker.com | sh \
+        && service docker enable \
+        && service docker start \
+        || returncode=$? \
+            && echo "problem installing docker!" \
+            && exit $returncode
+    python3 -m pip install -U pip
+    python3 -m pip install docker-compose
+}
+
+apt update \
+    && install_ubuntu \
+    || install_centos
+
 mkdir /home/scott/.ssh
 ssh-keygen -t ed25519 -f .ssh/id_ed25519 -q
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICa1xQPW/kFnPrO51Mp5gWpEpRZO8d6vtrWxIIpOoFd4 scott@scotts-server
