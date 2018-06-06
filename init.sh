fac141367e2270f4292486fbc8c1123c43cc9b03
scott <scott@tams.tech>
2018-06-05 22:35:39 -0400

    Added some comforts to init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 yum install epel-release
 yum install python34-pip python34-docker python34-pytest python34-PyYAML      \
-            python34-jinja2 docker docker-compose
+            python34-jinja2 docker docker-compose vim zsh rsync git
 mkdir /home/scott/.ssh
 ssh-keygen -t ed25519 -f .ssh/id_ed25519 -q
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICa1xQPW/kFnPrO51Mp5gWpEpRZO8d6vtrWxIIpOoFd4 scott@scotts-server
