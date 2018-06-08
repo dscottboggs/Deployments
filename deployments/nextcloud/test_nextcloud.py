dd8fbe00582cdb3d54c9d048c4cfd79caf859afc
scott <scott@tams.tech>
2018-06-08 01:22:38 -0400

    Tests for all URLs in user_info not just "s.cloud.tams.tech"

--- deployments/nextcloud/test_nextcloud.py
+++ deployments/nextcloud/test_nextcloud.py
@@ -9,25 +9,26 @@ from subprocess             import check_call
 
 def test_index():
     """Test that the root URLs respond properly."""
-    try:
-        response = get("http://s.cloud.tams.tech")
-        assert response.ok
-        assert response.history
-        assert response.history[0].status_code == 301
-        assert response.history[0].url == "http://s.cloud.tams.tech/"
-        assert response.history[1].status_code == 302
-        assert response.history[1].url == "https://s.cloud.tams.tech/"
-        assert response.status_code == 200
-        assert response.url == "https://s.cloud.tams.tech/login"
-        response = get("https://s.cloud.tams.tech")
-        assert response.ok
-        assert response.history
-        assert response.history[0].status_code == 302
-        assert response.history[0].url == "https://s.cloud.tams.tech/"
-        assert response.status_code == 200
-        assert response.url == "https://s.cloud.tams.tech/login"
-    except SSLError:
-        ...
+    for url in user_info['urls']:
+        try:
+            response = get("http://{}/".format(url))
+            assert response.ok
+            assert response.history
+            assert response.history[0].status_code == 301
+            assert response.history[0].url == "http://{}".format(url)
+            assert response.history[1].status_code == 302
+            assert response.history[1].url == "https://{}".format(url)
+            assert response.status_code == 200
+            assert response.url == "https://{}/login".format(url)
+            response = get("https://{}".format(url))
+            assert response.ok
+            assert response.history
+            assert response.history[0].status_code == 302
+            assert response.history[0].url == "https://{}".format(url)
+            assert response.status_code == 200
+            assert response.url == "https://{}/login".format(url)
+        except SSLError:
+            ...
 
 
 def test_user():
@@ -49,81 +50,81 @@ def test_upload():
     This test uses the backend WebDAV REST api and hence contains no
     checks for the actual GUI interface.
     """
-    try:
-        user = user_info['admin'][0]
-        url = "s.cloud.tams.tech"
-        response = put(
-            url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
-                url, user['user_id']
-            ),
-            auth=(user['user_id'], user['password']),
-            data=b"Test text text that tests."
-        )
-        assert response.ok
-        response = get(
-            url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
-                url, user['user_id']
-            ),
-            auth=(user['user_id'], user['password'])
-        )
-        assert response.ok
-        assert response.content == b"Test text text that tests."
-        response = delete(
-            url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
-                url, user['user_id']
-            ),
-            auth=(user['user_id'], user['password'])
-        )
-        assert response.ok
-    except:
-        ...
+    for url in user_info['urls']
+        try:
+            user = user_info['admin'][0]
+            response = put(
+                url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
+                    url, user['user_id']
+                ),
+                auth=(user['user_id'], user['password']),
+                data=b"Test text text that tests."
+            )
+            assert response.ok
+            response = get(
+                url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
+                    url, user['user_id']
+                ),
+                auth=(user['user_id'], user['password'])
+            )
+            assert response.ok
+            assert response.content == b"Test text text that tests."
+            response = delete(
+                url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
+                    url, user['user_id']
+                ),
+                auth=(user['user_id'], user['password'])
+            )
+            assert response.ok
+        except:
+            ...
 
 
 def test_upload_survives():
     """Test that an uploaded file survives container destruction."""
-    try:
-        user = user_info['admin'][0]
-        url = "s.cloud.tams.tech"
-        response = put(
-            url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
-                url, user['user_id']
-            ),
-            auth=(user['user_id'], user['password']),
-            data=b"Test text text that tests."
-        )
-        assert response.ok
-        response = get(
-            url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
-                url, user['user_id']
-            ),
-            auth=(user['user_id'], user['password'])
-        )
-        assert response.ok
-        assert response.content == b"Test text text that tests."
-        check_call(
-            "docker-compose down && docker volume prune -f"
-            + "&& docker-compose up -d",
-            shell=True,
-            cwd=THIS_DIR
-        )
-        wait(20)
-        response = get(
-            url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
-                url, user['user_id']
-            ),
-            auth=(user['user_id'], user['password'])
-        )
-        assert response.ok
-        assert response.content == b"Test text text that tests."
-        response = delete(
-            url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
-                url, user['user_id']
-            ),
-            auth=(user['user_id'], user['password'])
-        )
-        assert response.ok
-    except SSLError:
-        ...
+    for user in user_info['urls']:
+        try:
+            user = user_info['admin'][0]
+            response = put(
+                url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
+                    url, user['user_id']
+                ),
+                auth=(user['user_id'], user['password']),
+                data=b"Test text text that tests."
+            )
+            assert response.ok
+            response = get(
+                url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
+                    url, user['user_id']
+                ),
+                auth=(user['user_id'], user['password'])
+            )
+            assert response.ok
+            assert response.content == b"Test text text that tests."
+            check_call(
+                "docker-compose down && docker volume prune -f"
+                + "&& docker-compose up -d",
+                shell=True,
+                cwd=THIS_DIR
+            )
+            wait(20)
+            response = get(
+                url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
+                    url, user['user_id']
+                ),
+                auth=(user['user_id'], user['password'])
+            )
+            assert response.ok
+            assert response.content == b"Test text text that tests."
+            response = delete(
+                url="https://%s/remote.php/dav/files/%s/testfile.txt" % (
+                    url, user['user_id']
+                ),
+                auth=(user['user_id'], user['password'])
+            )
+            assert response.ok
+        except SSLError:
+            ...
 
 
 def test_container_presence():
