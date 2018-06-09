3e172bec150ab9690d650284ba7ebd3145186aae
scott <scott@tams.tech>
2018-06-09 16:21:53 -0400

    Fixed up init.sh

--- README.md
+++ README.md
@@ -3,14 +3,13 @@ Containerized deployments, tested with pytest, and secured with LetsEncrypt.
 
 Deploy in a single step on a CentOS7 or Ubuntu 18.04 system:
 
-    sudo su -c 'wget -O - https://raw.githubusercontent.com/dscottboggs/Deployments/master/init.sh | bash'
+    wget -O - https://raw.githubusercontent.com/dscottboggs/Deployments/master/init.sh | bash
 
 This installs the base dependencies with aptitude or yum, clones
 the repository into `./Deployments`, installs the python module,
 then finally performs the deployment and initial backup with
-`run.py`.
-
-(TODO uninstall script)
+`run.py`. Obviously you should review it and the script it pulls
+before piping it to your shell.
 
 I have been doing testing in an otherwise blank, up-to-date Ubuntu
 18.04 on qemu.
--- init.sh
+++ init.sh
@@ -1,44 +1,62 @@
 #!/usr/bin/env bash
 
+username=$USER
+
 function handle_error() {
     echo "Error while executing"
     echo $1
     exit $2
 }
 
-function install_ubuntu() {
-    apt install -y python3-pip python3-docker python3-pytest python3-jinja2 \
-        vim zsh rsync git apt-transport-https ca-certificates \
-        software-properties-common
-    wget -qO - https://get.docker.com | sh\
-        && systemctl enable docker \
-        && systemctl start docker \
+function install_docker_wget() {
+    sudo su -c ' \
+        wget -qO - https://get.docker.com | sh \
+            && systemctl enable docker  \
+            && systemctl start docker ' \
         || handle_error "Docker installation" $PIPESTATUS
-    python3 -m pip install -U pip
-    python3 -m pip install docker-compose PyYAML
-}
-function install_centos() {
-    yum install -y epel-release
-    yum install -y python34-pip python34-docker python34-pytest \
-        python34-PyYAML python34-jinja2 vim zsh rsync git
-    wget -qO - https://get.docker.com | sh \
-        && systemctl enable docker \
-        && systemctl start docker \
-        || handle_error "Docker installation" $PIPESTATUS
-    python3 -m pip install -U pip
-    python3 -m pip install docker-compose
 }
 
-apt update \
+function install_ubuntu() {
+    sudo apt install -y python3-pip python3-docker python3-pytest
+        python3-jinja2 vim zsh rsync git apt-transport-https ca-certificates \
+        software-properties-common
+    install_docker_wget
+    sudo python3 -m pip install -U pip
+    sudo python3 -m pip install docker-compose PyYAML
+}
+function install_centos() {
+    sudo yum install -y epel-release
+    sudo yum install -y python34-pip python34-docker python34-pytest \
+        python34-PyYAML python34-jinja2 vim zsh rsync git
+    install_docker_wget
+    sudo python3 -m pip install -U pip
+    sudo python3 -m pip install docker-compose
+}
+
+
+which sudo || apt install -y sudo || yum install -y sudo \
+    || handle_error "Installation of sudo" $?
+
+if [ $USER = "root" ]; then
+    echo "This script will elevate itself to SU privileges when necessary."
+    echo "Please run as a regular user."
+    exit 1
+fi
+
+sudo apt update  \
     && install_ubuntu \
     || install_centos
 
-mkdir /home/scott/.ssh
-ssh-keygen -t ed25519 -f .ssh/id_ed25519 -q
+mkdir $HOME/.ssh || handle_error "Creating $HOME/.ssh" $?
+ssh-keygen -t ed25519 -f $HOME/.ssh/id_ed25519 -q || handle_error "Creating cryptographic identity" $?
+chmod 700 $HOME/.ssh
+    && chmod 600 $HOME/.ssh/id_ed25519
+    && chmod 444 $HOME/.ssh/id_ed25519.pub
+    || handle_error "Changing permissions of the $HOME/.ssh/ dir" $?
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICa1xQPW/kFnPrO51Mp5gWpEpRZO8d6vtrWxIIpOoFd4 scott@scotts-server
-' > /home/scott/.ssh/authorized_keys
-cd /home/scott && \
+' > $HOME/.ssh/authorized_keys
+cd /opt && \
     git clone --recursive https://github.com/dscottboggs/Deployments.git
-cd /home/scott/Deployments      &&\
+cd /opt/Deployments      &&\
     python3 setup.py install   &&\
     python3 run.py
